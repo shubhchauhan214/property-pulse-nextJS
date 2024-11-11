68c732c780e0b9b79779308f82e408da058de412
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-11-11 22:57:13 +0530

    Remove .env from repository

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,16 @@
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
+import HomeProperties from '@/components/HomeProperties';
+
 
 const HomePage = () => {
+  
+  // console.log(process.env.MONGODB_URI); here we can check that our db is connected or not.
   return (
     <>
       <Hero />
       <InfoBoxes/>
+      <HomeProperties/>
     </>
   );
 };
