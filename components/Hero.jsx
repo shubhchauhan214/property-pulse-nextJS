f60eb5edb134907c1c552543ebd44ee70f5ede6d
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-11-19 00:30:36 +0530

    Property Details

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -19,7 +19,7 @@ const  Hero = () => {
             className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
           >
             <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
-              <label for="location" className="sr-only">Location</label>
+              <label htmlFor="location" className="sr-only">Location</label>
               <input
                 type="text"
                 id="location"
@@ -28,7 +28,7 @@ const  Hero = () => {
               />
             </div>
             <div className="w-full md:w-2/5 md:pl-2">
-              <label for="property-type" className="sr-only">Property Type</label>
+              <label htmlFor="property-type" className="sr-only">Property Type</label>
               <select
                 id="property-type"
                 className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
