e2cfc6c85e65adbf45c563f9a4d912ab361a03de
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-12-07 00:01:29 +0530

    Share buttons added

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -49,6 +49,10 @@ const BookmarkButton = ({ property }) => {
       toast.error("Something went wrong!");
     });
   };
+
+  if (loading){
+    return <p className="text-center">Loading...</p>
+  }
   return isBookmarked ? (
     <button className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center" onClick={handleClick}>
       <FaBookmark className="mr-2" />
--- components/ShareButton.jsx
+++ components/ShareButton.jsx
@@ -1,10 +1,57 @@
-import { FaShare } from "react-icons/fa";
+"use client";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  EmailIcon,
+} from "react-share";
 
-const ShareButton = ({property}) => {
+const ShareButton = ({ property }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property?._id}`;
+  const hashtag = property?.type
+  ? `#${property.type.replace(/\s/g, '')}ForRent`
+  : '';
+  const hashtags = hashtag ? [hashtag.slice(1)] : [];
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <FaShare className="mr-2"/> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2">
+        Share This Property:
+      </h3>
+      <div className="flex gap-3 justify-center pb-5">
+        <FacebookShareButton
+          url={shareUrl}
+          quote={property?.name || 'Check out this property!'}
+          hashtag={hashtag}
+        >
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={shareUrl}
+          title={property?.name || 'Check out this property!'}
+          hashtags={hashtags}
+        >
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+        <WhatsappShareButton
+          url={shareUrl}
+          title={property?.name || 'Check out this property!'}
+          separator="::"
+        >
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+        <EmailShareButton
+          url={shareUrl}
+          subject={property?.name || 'Check out this property!'}
+          body={`Check out this property listing: ${shareUrl}`}
+        >
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+      </div>
+    </>
   );
 };
 
